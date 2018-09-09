601138ffc0974591bdb30f85aea942c21ce49417
lifeprolific <thegregoman@gmail.com>
2018-09-09 14:45:57 -0400

    changed user resource to users resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :user, only: [:create, :update, :destroy, :index]
+      resources :users, only: [:create, :index]
       resource :session, only: :create
       resources :proclamations do
         resources :comments, only: [:index, :create]
