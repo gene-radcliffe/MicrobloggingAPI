5c776e333f89d60c1cc1d258f06ee04d434aef23
lifeprolific <thegregoman@gmail.com>
2018-09-09 14:39:29 -0400

    app controller and routes removed auth token from app controller removed signups from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
-      resource :user, only: [:update, :destroy]
-      resource :signup, only: :create
+      resource :user, only: [:create, :update, :destroy, :index]
       resource :session, only: :create
       resources :proclamations do
         resources :comments, only: [:index, :create]
